1824a7e3a20b79a11ccbe0ef6f20c1e33c83f9fc
Thulitha <138996396+Deshangaruda@users.noreply.github.com>
2026-04-15 18:17:13 +0530

    Add files via upload

--- Pass.js
+++ Pass.js
@@ -9,7 +9,7 @@
       const password = loginPass.value.trim();
       if(password === '001@Thuli_$Deshan:T1-M:T') {
         showToast('✅ Login successful! Redirecting...');
-        window.location.href ="admin Page.html";
+        window.location.href ="Admin data entry.html";
       } else {
         errorMessage.classList.remove('hidden1');
         loginPass.value = '';
